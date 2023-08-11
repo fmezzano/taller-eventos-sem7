2f18c1a578afaa68e1bef3b28a9fa58184068ab9
Frida Mezzano <friduchim@gmail.com>
2023-08-11 17:27:03 -0300

    Agregado de script y css
    Agrego archivos de JS y CSS siguiendo la consigna y nuevos eventos  de alertas.

---
+++ eventos.js
@@ -0,0 +1,8 @@
+const bttn = document.getElementById("bttn");
+const accionDiv = document.getElementById("divBttn");
+
+accionDiv.addEventListener("click", clickDiv);
+
+function clickDiv(){
+    alert("Hola! Soy el div");
+    }
